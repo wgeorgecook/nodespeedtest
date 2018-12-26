9e3cbbd8f454829b8a36f9358a08be6d7ad677f0
William George Cook <wgeorgecook@gmail.com>
2018-12-25 22:42:53 -0800

    Spin tweet into own file

--- speedtest.js
+++ speedtest.js
@@ -1,26 +1,14 @@
 'use strict';
 
-// Twitter data
-const Twitter = require('twitter');
-const settings = require('./settings')
-const client = new Twitter({
-  consumer_key:         settings.consumer_key,
-  consumer_secret:      settings.consumer_secret,
-  access_token_key:     settings.access_token,
-  access_token_secret:  settings.access_token_secret,
-});
 
 // Speedtest data
 const speedTest = require('speedtest-net');
-const test = speedTest({maxTime: 5000});
-let results = {
-    download: null,
-    upload: null
-};
 
-let getResults = (tester) =>  new Promise(function(resolve, reject) {
+
+let getResults = () =>  new Promise(function(resolve, reject) {
     try {
-        tester.on('data', data => {
+        let test = speedTest({maxTime: 5000});
+        test.on('data', data => {
             resolve(data.speeds);
         });
     } catch (err) {
@@ -31,30 +19,24 @@ let getResults = (tester) =>  new Promise(function(resolve, reject) {
 
 
 
-async function updateResults(tester) {
+async function updateResults() {
+    let results = {
+        download: null,
+        upload: null
+    };
+
     try {
-        const data = await getResults(tester)
+        const data = await getResults()
         results.download = data.download;
         results.upload = data.upload;
-        console.log(results)
+        return results;
     } catch (err) {
         console.log("Error: ")
         console.log(err)
     }
 }
 
+// updateResults();
 
 
-
-async function postTweet(tester) {
-    await updateResults(test);
-    client.post('statuses/update', {status: `Down: ${results.download}. Up: ${results.upload}`})
-    .then( tweet => console.log("Successfully posted!"))
-    .catch( (err) => console.log(err))
-
-}
-
-postTweet(test)
-
-module.exports = updateResults;
-module.exports = postTweet;
+module.exports.updateResults = updateResults;
