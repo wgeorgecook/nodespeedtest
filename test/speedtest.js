25e8ed2a673ac4945440708fbc60d9dee622d216
William George Cook <wgeorgecook@gmail.com>
2018-12-25 22:00:45 -0800

    Testing suites

---
+++ test/speedtest.js
@@ -0,0 +1,18 @@
+describe('Speedtests', function() {
+
+    it('Gets a download value', function() {
+        assert.equal(true, true);
+    });
+
+    it ('Gets an upload value', function() {
+        assert.equal(true, true);
+    });
+
+});
+
+describe('Twitter', function() {
+
+    it('Authenticates', function() {
+        assert.equal(true, true);
+    })
+});
